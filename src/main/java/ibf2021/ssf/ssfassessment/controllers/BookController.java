10f31189535970be22514980eb1f798a32c7a9a2
Mac <mac@Macs-MacBook-Pro.local>
2022-01-18 09:56:55 +0800

    task4part1

--- src/main/java/ibf2021/ssf/ssfassessment/controllers/BookController.java
+++ src/main/java/ibf2021/ssf/ssfassessment/controllers/BookController.java
@@ -1,8 +1,19 @@
 package ibf2021.ssf.ssfassessment.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(path="/book")
 public class BookController {
     
+    @GetMapping
+    public String searchBook(@RequestParam String query, Model model) {
+        model.addAttribute("results", query);
+
+        return "search";
+    }
 }
